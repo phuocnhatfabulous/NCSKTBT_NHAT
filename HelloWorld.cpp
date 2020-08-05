fd7b1c888054767a75af0926bedea8d9192eeb72
phuocnhatfabulous <69050828+phuocnhatfabulous@users.noreply.github.com>
2020-08-05 20:21:27 +0700

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NCSKTBT_NHAT.cool
+//
+//  Created by Đoàn Nhật on 8/5/20.
+//  Copyright © 2020 Đoàn Nhật. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
