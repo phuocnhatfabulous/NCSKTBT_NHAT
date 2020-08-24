76c83639bb8bbbf34a2723de371360b6b3183769
phuocnhatfabulous <69050828+phuocnhatfabulous@users.noreply.github.com>
2020-08-24 19:21:51 +0700

    Add files via upload

---
+++ person.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+class Person{
+    string id;
+public:
+//    default constructor
+    Person(){
+        id = "000";
+    }
+    //Argument constructor
+    Person(string id){
+        this->id = id;
+    }
+    //Copy constructor
+    Person(const Person &t){
+        cout<<"Copy constructor called"<<endl;
+    }
+    //assignment operator
+    Person& operator = (const Person &t){
+        cout<<"Assignment operator called"<<endl;
+        return *this;
+    }
+};
+//Driver code
+int main(){
+    Person person1;
+    Person t1, t2;
+    t2 = t1;
+    Person t3 = t1;
+    getchar();
+    return 0;
+}
